e6f0a401517b948ad752839d6846e01d76e7d0f8
Steven Huang <steven.charles.huang@gmail.com>
2019-11-30 16:35:28 -0500

    added skeleton tests for postsapicontroller

---
+++ src/test/java/com/example/postsapi/controller/PostsApiControllerTest.java
@@ -0,0 +1,91 @@
+package com.example.postsapi.controller;
+
+import com.example.postsapi.bean.CommentBean;
+import com.example.postsapi.bean.UserBean;
+import com.example.postsapi.model.Post;
+import com.example.postsapi.service.PostServiceImpl;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@EnableAutoConfiguration(exclude = { SecurityAutoConfiguration.class})
+public class PostsApiControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private PostServiceImpl postService;
+
+    @InjectMocks
+    private Post post;
+
+    @InjectMocks
+    private UserBean user;
+
+    @InjectMocks
+    private CommentBean comment;
+
+    private List<Post> postList;
+
+    @Before
+    public void init() {
+        user.setUsername("testUser");
+
+        comment.setId(1);
+        comment.setText("Test Comment Text");
+        comment.setPostId(1);
+        comment.setUserId(1);
+
+        post.setId(1);
+        post.setTitle("Test Post Title");
+        post.setDescription("Test Post Description");
+        post.setUser_id(1);
+        post.setUser(user);
+
+        postList = new ArrayList<>();
+        postList.add(post);
+    }
+
+    @Test
+    public void createPost_ReturnPost_Success() throws Exception {}
+
+    @Test
+    public void getAllPosts_ReturnPostList_Success() throws Exception {}
+
+    @Test
+    public void getPostsByUserId_ReturnPostList_Success() throws Exception {}
+
+    @Test
+    public void deletePost_ReturnStringMsg_Success() throws Exception {}
+
+    @Test
+    public void getCommentsByPostId_ReturnsCommentsList_Success() {}
+
+    @Test
+    public void findPostById_ReturnsPost_Success() {}
+}
