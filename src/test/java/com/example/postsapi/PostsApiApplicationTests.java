d469bbc2c63190d875f07d73a2b3b41e7beafd94
Steven Huang <steven.charles.huang@gmail.com>
2019-12-01 12:44:20 -0500

    added test back to PostsApiApplicationTests

--- src/test/java/com/example/postsapi/PostsApiApplicationTests.java
+++ src/test/java/com/example/postsapi/PostsApiApplicationTests.java
@@ -1,13 +1,14 @@
 package com.example.postsapi;
 
-//import org.junit.jupiter.api.Test;
+//import org.junits.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PostsApiApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
+
+	@Test
+	void contextLoads() {
+	}
 
 }
