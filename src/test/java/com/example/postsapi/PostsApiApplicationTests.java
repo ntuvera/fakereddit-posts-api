7ff5029497b8ba785e25c5a6b96655a7f171eae7
Steven Huang <steven.charles.huang@gmail.com>
2019-12-01 12:27:29 -0500

    removed jupiter again, downgrade surefire 2.22.2 -> 2.19.1, removed redundant spring-boot-starter-data-jpa

--- src/test/java/com/example/postsapi/PostsApiApplicationTests.java
+++ src/test/java/com/example/postsapi/PostsApiApplicationTests.java
@@ -1,13 +1,13 @@
 package com.example.postsapi;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PostsApiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
+//
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
